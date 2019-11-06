42db678e5daea467b62fea99fdb1d10173e01efb
huangshuang <hs183.qq.com>
2019-11-06 23:20:30 +0800

    初步将服务注册到eureka

--- microservice-provider-user/src/main/java/com/hs/cloud/microserviceprovideruser/MicroserviceProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/hs/cloud/microserviceprovideruser/MicroserviceProviderUserApplication.java
@@ -2,9 +2,14 @@ package com.hs.cloud.microserviceprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceProviderUserApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceProviderUserApplication.class, args);
--- microservice-provider-user/src/main/java/com/hs/cloud/microserviceprovideruser/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/hs/cloud/microserviceprovideruser/controller/UserController.java
@@ -3,7 +3,13 @@ package com.hs.cloud.microserviceprovideruser.controller;
 
 import com.hs.cloud.microserviceprovideruser.dao.UserDao;
 import com.hs.cloud.microserviceprovideruser.entity.User;
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private static Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserDao userDao;
 
+    /*@Autowired
+    private DiscoveryClient discoveryClient;*/
+
+
+
     @GetMapping("/user/info/{id}")
     public User getUserInfo(@PathVariable Long id){
+
+        /*ApplicationInfoManager infoManager =discoveryClient.getApplicationInfoManager();
+
+        InstanceInfo instanceInfo =infoManager.getInfo();
+
+        LOG.info("user host:"+instanceInfo.getHostName()+",service_id:"+instanceInfo.getId());*/
+
         return this.userDao.getOne(id);
     }
 }
