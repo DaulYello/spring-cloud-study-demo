eed7f1ea5bd536d0383c37a28447d509eb6d9b18
huangshuang <hs183.qq.com>
2019-11-06 22:12:11 +0800

    eureka访问成功！并且在用户测试模块里面加上了一个jsoup方式爬虫的dome

--- microservice-provider-user/src/test/java/com/hs/cloud/microserviceprovideruser/MicroserviceProviderUserApplicationTests.java
+++ microservice-provider-user/src/test/java/com/hs/cloud/microserviceprovideruser/MicroserviceProviderUserApplicationTests.java
@@ -1,13 +1,49 @@
 package com.hs.cloud.microserviceprovideruser;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-@SpringBootTest
+import java.io.IOException;
+
+/*@SpringBootTest*/
 class MicroserviceProviderUserApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+
+	/**
+	 * jsoup方式 获取虎扑新闻列表页
+	 * @param url 虎扑新闻列表页url
+	 */
+	public static void jsoupList(String url){
+		try {
+			Document document = Jsoup.connect(url).get();
+			// 使用 css选择器 提取列表新闻 a 标签
+			// <a href="https://voice.hupu.com/nba/2484553.html" target="_blank">霍华德：夏休期内曾节食30天，这考验了我的身心</a>
+			Elements elements = document.select("div.news-list > ul > li > div.list-hd > h4 > a");
+			for (Element element:elements){
+//                System.out.println(element);
+				// 获取详情页链接
+				String d_url = element.attr("href");
+				// 获取标题
+				String title = element.ownText();
+
+				System.out.println("详情页链接："+d_url+" ,详情页标题："+title);
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		String url = "https://voice.hupu.com/nba";
+
+		jsoupList(url);
 	}
 
 }
+
